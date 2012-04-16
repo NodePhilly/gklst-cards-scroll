e64b8d9ee3f18bef531881adf62ce17c3f061ec5
Tim Savery <tim.savery@gmail.com>
2012-04-16 17:53:20 -0400

    twitter

--- twitter.js
+++ twitter.js
@@ -3,37 +3,33 @@ var twitter = require('ntwitter')
   , credentials = require('./twitter-creds.json');
 
 exports.init = function(callback) {
-  var twit = new twitter({
-    consumer_key: 'f634xNy9wp9VcoY5V8ygA',
-	consumer_secret: '2stzJoEjXiN8nvynN3eL9YroIdAbsjlLGmT2M3gzs',
-	access_token_key: '104793047-luKK7cCdk3no6AZJH79i4R3RGsMLDUuBGxUTsmZG',
-	access_token_secret: 'BtpogtuaSHEjoSsRp8nT9DCz6wPfkzGEoHmPMDhk'
-  });
+  var twit = new twitter(credentials);
 
-  var stream = new EventEmitter();
+  var twitStream = new EventEmitter();
 
-  twit.search('#nodephilly OR @NodePhilly', function(err, data) {
-    if (err) { 
-      console.log(err);
-      return; 
-    }
-    
-    for (var i=0; i<data.results.length; i++) {
-      stream.emit('tweet', {
-        id: data.results[i].id,
+  twit.stream('statuses/filter', { track: '#obama' }, function(stream) {
+    stream.on('data', function(data) {
+      twitStream.emit('tweet', {
+        id: data.id,
         user: {
-          screen_name: data.results[i].from_user
-        },            
-        text: data.results[i].text
+          avatar: data.user.profile_image_url,
+          screen_name: data.user.screen_name
+        },
+        text: data.text
       });
-    }
-    
-    twit.stream('statuses/filter', { track: '#nodephilly' }, function(stream) {
-	  stream.on('data', function(data) {
-	    io.sockets.emit('tweet', data);
-	  });
-	});
+    });
+
+    stream.on('error', function(data) {
+      twitStream.emit('tweet', {
+        id: data.id,
+        user: {
+          avatar: data.user.profile_image_url,
+          screen_name: data.user.screen_name
+        },
+        text: data.text
+      });
+    });
   });
 
-  callback(stream);
+  callback(twitStream);
 };
