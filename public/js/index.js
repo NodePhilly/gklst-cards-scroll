eb2d316ca6eea43fec0cde4e25e63e3c9074e41c
Tim Savery <tim.savery@gmail.com>
2012-04-21 16:18:59 -0400

    scrolling hack

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,13 @@
 // ** DEPENDENCIES:
-// **   1) Queue.compressed.js
 // **
+// **   1) Queue.compressed.js
 // **
 
 $(document).ready(function() {	
-	loadCardsQueue(function() {
-	  setInterval(scrollCards, 3000);
-	});
+  loadCardsQueue(function() {	  
+    scrollCards();
+    scrollTweets();
+  });
 });
 
 // ################  GEEKLIST
@@ -38,35 +39,60 @@ function loadCardsQueue(callback) {
 };
 
 function scrollCards() {
-  var firstCardClone = $('#geeklist section:first').clone();
+  var lastCard = $('#geeklist .tile:last');
+  var lastCardClone = lastCard.clone();
 
-  $('#geeklist section:first').slideUp(1000, function() {
-  	$(this).remove();
-  	$('#geeklist').append(firstCardClone);
+  lastCardClone.hide().prependTo($('#geeklist')).fadeIn(1500, function() {
+  	lastCard.remove();
+
+  	setTimeout(scrollCards, 3000);  	
   });
 };
 
 // ################  TWEETS
+var tweetQueue = new Queue();
+
+function createTweetElement(tweet) {
+	return $('\
+	  <section class="tile" id="' + tweet.id + '">\
+	    <section class="user-image">\
+	      <img src="' + tweet.user.avatar + '" />\
+	      <div class="clear" />\
+	    </section>\
+	    <section class="right">\
+	      <h4>' + tweet.text + '</h4>\
+	      <p>@' + tweet.user.screen_name + '</p>\
+	      <div class="clear" />\
+	    </section>\
+	    <div class="clear" />\
+	  </section>');
+};
+
 var socket = io.connect('/');
 socket.on('tweet', function(tweet) {
-	var markup = '\
-		<section class="tile" id="' + tweet.id + '" style="display: none;">\
-		    <section class="user-image">\
-		      <img src="' + tweet.user.avatar + '" />\
-		    </section>\
-		    <section class="right">\
-		      <h4>' + tweet.text + '</h4>\
-		      <p>@' + tweet.user.screen_name + '</p>\
-		    </section>\
-		    <br class="clear" />\
-		</section>';
-	
-	$('#tweets').queue(function() {
-		var container = $(this);
-		container.prepend(markup);
-
-		$('#' + tweet.id).fadeIn(1000, function() {
-			container.dequeue();
-		});
-	});
+	tweetQueue.enqueue(tweet);
 });
+
+function scrollTweets() {
+  var newTweet = tweetQueue.dequeue();
+
+  if (newTweet) {
+  	var tweetElement = createTweetElement(newTweet);
+  	tweetElement.hide().prependTo($('#tweets')).fadeIn(2000, function() {
+  	  setTimeout(scrollTweets, 3000);
+  	});
+  } else {
+    var lastTweet = $('#tweets .tile:last');
+
+    if (lastTweet.length > 0) {
+      var lastTweetClone = lastTweet.clone();
+
+      lastTweetClone.hide().prependTo($('#tweets')).fadeIn(2000, function() {
+  	    lastTweet.remove();
+  	    setTimeout(scrollTweets, 3000);
+      });
+    } else {
+    	setTimeout(scrollTweets, 3000);
+    }
+  }
+};
